2becfaa0659f91369789e732b5d7544813be71d1
davidemioma <davidemiomauche@gmail.com>
2021-12-27 16:00:25 +0100

    page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Header from "./components/Header/Header";
 import Todo from "./components/Todo/Todo";
 import Footer from "./components/Footer/Footer";
-import { useState } from "react";
 import TodoProvider from "./store/TodoProvider";
 
 function App() {
